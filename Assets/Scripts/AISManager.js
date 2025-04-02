82d3d6ca5ba3a667c435da31649cee5c9b2feb9e
Gerard Llorach <gerard.llorach@gmail.com>
2025-04-02 12:56:43 +0200

    added aismanager. now need to connect to map and 3D

---
+++ Assets/Scripts/AISManager.js
@@ -0,0 +1,167 @@
+
+import shipTypes from './AISShipTypes.js';
+
+
+export class AISManager {
+
+  ships = {}; // Dictionary to store ship objects
+
+  constructor() {
+    // Connection
+    this.ws;
+    // if (window.location.href.includes('localhost') || window.location.href.includes('127.0.0.1')) {
+    //   this.ws = new WebSocket('ws://localhost:30521');
+    // } else {
+      this.ws = new WebSocket('wss://api.icatmar.cat/ais');
+    // }
+
+    // General connection events
+    // OPEN
+    this.ws.onopen = () => {
+      window.eventBus.emit('AISManager_wsConnected');
+      console.log('✅ WebSocket connection established');
+      // Subscribe to the desired channel
+      this.ws.send(JSON.stringify({ action: 'subscribe', id: 'boiasomorrostro' }));
+    }
+    // CLOSE
+    this.ws.onclose = () => {
+      window.eventBus.emit('AISManager_wsClosed');
+      console.log('WebSocket connection closed');
+    }
+    // ERROR
+    this.ws.onerror = (error) => {
+      window.eventBus.emit('AISManager_wsError', error);
+      console.error('WebSocket error:', error);
+    }
+
+    // MESSAGE
+    this.ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      
+      // Add bounding boxes
+      if (data.bboxes) {
+      }
+
+      // Process AIS message
+      if (!data.message || !data.message.Message) return;
+      this.processAISMessage(data.message);
+    }
+  }
+
+
+  processAISMessage(message) {
+
+    const metaData = message.MetaData;
+    if (!metaData || !metaData.latitude || !metaData.longitude) { debugger };
+
+    const MMSI = metaData.MMSI_String;
+    // Create ship
+    if (!this.ships[MMSI]) {
+      this.ships[MMSI] = { MMSI: MMSI };
+      window.eventBus.emit('AISManager_shipCreated', metaData);
+    }
+
+
+    // Default values
+    const shipName = metaData.ShipName || this.ships[MMSI].shipName || "Unknown Ship";
+    const lat = metaData.latitude;
+    const long = metaData.longitude;
+
+    let heading = this.ships[MMSI].heading || 511; // A value of 511 indicates no heading: https://api.vtexplorer.com/docs/response-ais.html
+    let sog = this.ships[MMSI].sog || 0;
+    // https://api.vtexplorer.com/docs/ref-aistypes.html
+    let shipType = this.ships[MMSI].shipType || "Unknown"
+    // https://api.vtexplorer.com/docs/response-ais.html
+    let dimensions = this.ships[MMSI].dimensions || { A: -1, B: -1, C: -1, D: -1 };
+    let typeOfMessages = this.ships[MMSI].typeOfMessages || [];
+
+
+    // Get information from the message
+    // Position Report
+    if (message.Message.PositionReport) {
+      const positionReport = message.Message.PositionReport;
+      heading = positionReport.TrueHeading;
+      sog = positionReport.Sog;
+      if (typeOfMessages.includes("PositionReport") == false) typeOfMessages.push("PositionReport");
+      this.ships[MMSI].heading = heading;
+      this.ships[MMSI].sog = sog;
+      
+    }
+    // Static Ship Data
+    if (message.Message.ShipStaticData) {
+      const shipStaticData = message.Message.ShipStaticData;
+      shipType = shipStaticData.Type;
+      dimensions = shipStaticData.Dimension;
+      if (typeOfMessages.includes("ShipStaticData") == false) typeOfMessages.push("ShipStaticData");
+    }
+    // StandardClassBPositionReport
+    if (message.Message.StandardClassBPositionReport) {
+      const standardClassBPositionReport = message.Message.StandardClassBPositionReport;
+      heading = standardClassBPositionReport.TrueHeading;
+      sog = standardClassBPositionReport.Sog;
+      if (typeOfMessages.includes("StandardClassBPositionReport") == false) typeOfMessages.push("StandardClassBPositionReport");
+    }
+    // Other kind of messages
+    if (!message.Message.ShipStaticData && !message.Message.PositionReport && !message.Message.StandardClassBPositionReport) {
+      let messageType = Object.keys(message.Message)[0];
+      if (typeOfMessages.includes(messageType) == false) typeOfMessages.push(messageType);
+    }
+
+    // Infer heading from position change if not available
+    if (heading == 511 && this.ships[MMSI].latitude && this.ships[MMSI].longitude) {
+      heading = this.calculateHeading(this.ships[MMSI].longitude, this.ships[MMSI].latitude, long, lat);
+      this.ships[MMSI].empiricHeading = heading;
+    }
+
+
+    // Assign new lat long
+    this.ships[MMSI].latitude = lat;
+    this.ships[MMSI].longitude = long;
+    // Assign values to ship
+    this.ships[MMSI].shipName = shipName;
+    this.ships[MMSI].shipTypeId = shipType;
+    this.ships[MMSI].shipType = shipTypes[shipType];
+    this.ships[MMSI].sog = sog;
+    this.ships[MMSI].heading = heading;
+    this.ships[MMSI].dimensions = dimensions;
+    this.ships[MMSI].length = dimensions.A + dimensions.B;
+    this.ships[MMSI].beam = dimensions.C + dimensions.D;
+    this.ships[MMSI].lastTmst = metaData.time_utc;
+    this.ships[MMSI].typeOfMessages = typeOfMessages;
+
+
+    // Emit event to update ship data
+    window.eventBus.emit('AISManager_receivedAISMessage', this.ships[MMSI]);
+  }
+
+
+
+
+
+  // Calculate the heading between two points
+  calculateHeading(oldLong, oldLat, newLong, newLat) {
+    // Convert degrees to radians
+    const toRadians = (deg) => (deg * Math.PI) / 180;
+    const toDegrees = (rad) => (rad * 180) / Math.PI;
+
+    const lat1 = toRadians(oldLat);
+    const lat2 = toRadians(newLat);
+    const deltaLong = toRadians(newLong - oldLong);
+
+    // Calculate the bearing
+    const y = Math.sin(deltaLong) * Math.cos(lat2);
+    const x =
+      Math.cos(lat1) * Math.sin(lat2) -
+      Math.sin(lat1) * Math.cos(lat2) * Math.cos(deltaLong);
+    let bearing = toDegrees(Math.atan2(y, x));
+
+    // Normalize the bearing to 0° - 360°
+    return (bearing + 360) % 360;
+  }
+
+
+
+
+}
+
+export default AISManager;
