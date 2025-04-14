bfaeabd69065d3914f5d87d97a23fb578e1d7410
Gerard Llorach <gerard.llorach@gmail.com>
2025-04-14 11:56:30 +0200

    integrated weather widget. now working on cmems connection

--- Components/BottomSection/Data.vue
+++ Components/BottomSection/Data.vue
@@ -2,26 +2,45 @@
   <div class="content">
 
     <!-- BUOY -->
+    <h3>Buoy</h3>
+    <!-- Loading -->
+    <div v-if="isLoading" class="loading">Loading...</div>
 
     <!-- HF Radar -->
+    <h3>High Frequency Radar</h3>
+    <!-- Loading -->
+    <div v-if="isLoading" class="loading">Loading...</div>
 
     <!-- CMEMS -->
+    <h3>CMEMS</h3>
+    <!-- Loading -->
+    <div v-if="isLoading" class="loading">Loading...</div>
+    <WeatherWidget></WeatherWidget>
 
     <!-- AIS -->
+    <h3>Vessels from AIS</h3>
+    <!-- Loading -->
+    <div v-if="isLoading" class="loading">Loading...</div>
 
   </div>
 </template>
 
 
 <script>
+import WeatherWidget from "./DataSections/WeatherWidget.vue";
+
 export default {
   name: "Data",
   data() {
     return {
       isLoading: true, // Flag to indicate loading state
+      exists: false, // Flag to indicate if data exists
+      isConnected: false, // Flag to indicate if connected to the buoy
+      hasData: false, // Flag to indicate if data is available
     }
   },
   mounted(){
+
 
   },
   methods: {
@@ -34,7 +53,10 @@ export default {
         this.isLoading = false; // Set loading to false after fetching
       }
     },
-  }
+  },
+  components: {
+    WeatherWidget,
+  },
 }
 </script>
 
@@ -44,7 +66,8 @@ export default {
   margin: min(20px, 2vw);
   overflow: auto;
   font-size: small;
-  max-width: 60vw;
+  max-width: 80vw;
   max-height: 90%;
+  width: 100%;
 }
 </style>
